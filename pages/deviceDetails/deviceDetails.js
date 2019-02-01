c033a1dbcc7190b248ed089f7099f211180d81a4
wjwgit <w417287508@163.com>
2019-02-01 16:37:06 +0800

    提交更新
    提交更新

--- pages/deviceDetails/deviceDetails.js
+++ pages/deviceDetails/deviceDetails.js
@@ -1,66 +1,101 @@
+const app = getApp()
+const comApi = app.api;
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-   
+    deviceData: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
- 
+  onLoad: function(options) {
+    let deviceData = [];
+    comApi.getDeviceInfo('51052216080302').then(res => {
+      console.log('getDeviceInfo', res)
+      if (res && res.IsSuccess) {
+        if (res.Data) {
+          var thisData = res.Data;
+          thisData.map(function (items) {
+            let obj={
+              Name: items.Name,
+              child: []
+            }
+            items.TestComponents.map(function(item){
+              obj.child.push({
+                AnalyzerName: item.AnalyzerName,
+                AnalyzerPrinciple: item.AnalyzerPrinciple,
+                AnalyzerRange: item.AnalyzerRange,
+                ComponentName: item.ComponentName,
+                DeviceModel: item.DeviceModel,
+                MeasurementUnit: item.MeasurementUnit,
+                Slope: item.Slope,
+                Intercept: item.Intercept
+              })
+            })
+            deviceData.push(obj)
+          })
+          console.log(this.data.deviceData);
+          this.setData({
+            deviceData:deviceData
+          })
+          console.log(this.data.deviceData);
+        }
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-    
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-    
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-    
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-    
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-    
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-    
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-    
+  onShareAppMessage: function() {
+
   },
   onChange(e) {
 
