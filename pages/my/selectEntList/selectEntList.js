a86844371f43cce6133684d89e59e9d9339895c5
unknown <unknown@example.com>
2019-08-21 10:38:29 +0800

    0

--- pages/my/selectEntList/selectEntList.js
+++ pages/my/selectEntList/selectEntList.js
@@ -59,9 +59,6 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-      // wx.setNavigationBarTitle({
-      //   title: pointName,
-      // })
     this.getUserEntInfo();
   },
 
