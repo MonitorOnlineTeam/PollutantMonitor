b4d1c7b037b5430b7d932c5a0e6d0539644d3aa7
wjwgit <w417287508@163.com>
2019-11-25 09:16:43 +0800

    update

---
+++ pages/qca/opendoor/opendoor.js
@@ -0,0 +1,79 @@
+// pages/qca/opendoor/opendoor.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    textareaBValue: '',
+    array: ['请选择','更换标气', '维护备件', '设备维修', '设备巡检'],
+    index: 0,
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  },
+  textareaBInput(e) {
+    this.setData({
+      textareaBValue: e.detail.value
+    })
+  },
+  bindPickerChange: function(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  }
+})
