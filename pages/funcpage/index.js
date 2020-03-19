ada0cf95889268fb195cce4fe28dae31a5857c2e
wjwgit <w417287508@163.com>
2020-03-19 10:34:59 +0800

    提交更新

--- pages/funcpage/index.js
+++ pages/funcpage/index.js
@@ -21,6 +21,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
+    common.setStorage("ApiType", 1);
     this.myComponent = this.selectComponent('#myComponent');
   },
 
--- pages/home/index.js
+++ pages/home/index.js
@@ -22,6 +22,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
+    common.setStorage("ApiType", 1);
     var that = this;
     app.wxLogin(function() {
       comApi.initTicket(function() {
